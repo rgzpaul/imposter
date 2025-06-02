f03f404e6312aa3195b24351c610809e16859683
Paul Rodriguez <71314522+rgzpaul@users.noreply.github.com>
2025-06-02 20:11:43 +0200

    fix fool key

--- assets/server.php
+++ assets/server.php
@@ -7,7 +7,8 @@ header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
 $parole_file = 'list.json';
 $config_file = 'config.json';
 
-function caricaJson($file) {
+function caricaJson($file)
+{
     if (!file_exists($file)) {
         return ['giocatori' => [], 'parola' => '', 'brutal_mode' => false, 'game_id' => ''];
     }
@@ -18,7 +19,8 @@ function caricaJson($file) {
     return json_decode($content, true);
 }
 
-function salvaJson($file, $data) {
+function salvaJson($file, $data)
+{
     $fp = fopen($file, 'w');
     if (flock($fp, LOCK_EX)) { // Blocca il file per la scrittura
         fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
@@ -30,25 +32,27 @@ function salvaJson($file, $data) {
     }
 }
 
-function generaGameId() {
+function generaGameId()
+{
     return chr(random_int(65, 90)) . random_int(1, 9);
 }
 
-function assegnaNumeriCasuali($giocatori) {
+function assegnaNumeriCasuali($giocatori)
+{
     $numeriDisponibili = range(1, count($giocatori));
     shuffle($numeriDisponibili);
-    
+
     foreach ($giocatori as $index => &$giocatore) {
         $giocatore['numero'] = array_pop($numeriDisponibili);
     }
     unset($giocatore);
-    
+
     return $giocatori;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'];
-    
+
     if ($action == 'join') {
         $nickname = trim($_POST['nickname']);
         $configData = caricaJson($config_file);
@@ -78,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $nickname = trim($_POST['nickname']);
         $configData = caricaJson($config_file);
 
-        $configData['giocatori'] = array_filter($configData['giocatori'], function($giocatore) use ($nickname) {
+        $configData['giocatori'] = array_filter($configData['giocatori'], function ($giocatore) use ($nickname) {
             return strcasecmp($giocatore['nickname'], $nickname) != 0;
         });
         $configData['giocatori'] = array_values($configData['giocatori']);
@@ -109,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $configData['giocatori'] = assegnaNumeriCasuali($configData['giocatori']);
 
             $indice_impostore = array_rand($configData['giocatori']);
-            
+
             if ($configData['brutal_mode']) {
                 $giocatoreConNumero1 = null;
                 foreach ($configData['giocatori'] as $index => &$giocatore) {
@@ -127,21 +131,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 } while ($configData['giocatori'][$indice_impostore]['numero'] == 1);
             }
 
+            // First assign all players as "buono"
             foreach ($configData['giocatori'] as $index => &$giocatore) {
                 $giocatore['ruolo'] = ($index == $indice_impostore) ? 'impostore' : 'buono';
-                $giocatore['is_fool'] = false; // Initialize is_fool to false
             }
-    
+
             // Randomly select one good player to be the fool
-            $goodPlayersIndices = array_keys(array_filter($configData['giocatori'], function($g) {
+            $goodPlayersIndices = array_keys(array_filter($configData['giocatori'], function ($g) {
                 return $g['ruolo'] === 'buono';
             }));
-    
+
             if (!empty($goodPlayersIndices)) {
                 $foolIndex = $goodPlayersIndices[array_rand($goodPlayersIndices)];
-                $configData['giocatori'][$foolIndex]['is_fool'] = true;
+                $configData['giocatori'][$foolIndex]['ruolo'] = 'fool';
             }
-    
+
             unset($giocatore);
 
             $parole = caricaJson($parole_file)['parole'];
@@ -203,11 +207,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         // Trova la parola e il ruolo per il nickname
         $word = '';
         $role = '';
-        $isFool = false;
         foreach ($configData['giocatori'] as $giocatore) {
             if (strcasecmp($giocatore['nickname'], $nickname) == 0) {
                 $role = $giocatore['ruolo'];
-                $isFool = isset($giocatore['is_fool']) ? $giocatore['is_fool'] : false;
+                // Both fool and buono players see the word
                 $word = ($giocatore['ruolo'] === 'impostore') ? '???' : $configData['parola'];
                 break;
             }
@@ -217,8 +220,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             'giocatori' => $configData['giocatori'],
             'game_id' => $configData['game_id'],
             'word' => $word,
-            'role' => $role,
-            'is_fool' => $isFool
+            'role' => $role
         ]);
     }
 
@@ -232,4 +234,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         ]);
     }
 }
-?>
